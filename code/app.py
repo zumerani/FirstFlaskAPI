2a8d103e9f334748573232393865304ca335ad9a
Zain Umerani <zumerani@gmail.com>
2016-12-30 18:10:07 -0800

    Added status codes and cleaned up code (lambda functions)

--- code/app.py
+++ code/app.py
@@ -10,16 +10,21 @@ items = [] #Contains a dictionary for each item
 
 class Item(Resource): #'Item' will inherit from 'Resource'
     def get(self , name): #This resource can only be accessed with 'get' ... if you want 'post' add a post method
-        for item in items:
-            if item['name'] == name:
-                return item #We are using flask_restful, so no need to use JSONIFY, it is done for you.
-        return {'item' : None }, 404
+        item = next( filter(lambda x : x['name'] == name , items ) , None ) # This returns a filter object, so we can use 'next' to
+        return item, 200 if item is not None else 404                       # get the first found item. We can called 'next' many
+                                                                            # times. The second parameter 'None' is used to return
+                                                                            # 'None' if 'filter' cannot find an item. Performing
+                                                                            # 'next' on something not found will produce an error.
+
 
     def post(self , name):
-        data = request.get_json() #When someone sends a request, it is in the 'request' object
-        item = { 'name' : name , 'price' : data['price'] }
-        items.append(item)
-        return item, 201
+        if next( filter(lambda x : x['name'] == name , items ) , None ) is not None: #Item exists
+            return { 'message' : 'Item: {} already exists.'.format(name) }, 400
+        else:
+            data = request.get_json() #When someone sends a request, it is in the 'request' object
+            item = { 'name' : name , 'price' : data['price'] }
+            items.append(item)
+            return item, 201
 
 
 class ItemList(Resource):
