34ab02e6817ea1e14fb177a65b9c9bd4dd80799f
Zain Umerani <zumerani@gmail.com>
2016-12-30 16:35:07 -0800

    Added app.py and edited README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FirstFlaskAPI
+
+This is a small project to learn the Flask Web Framework. The project involves building an API (app.py) that has the ability to create, read, delete, and update items in a list. The list is local, and is not saved anywhere externally.
---
+++ code/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask_restful import Resource, Api #Resource is just a 'thing' that our API can handle with or return
+
+#Flask-RESTful is an extension for Flask that adds support for quickly building REST APIs.
+
+app = Flask(__name__)
+api = Api(app)
+
+items = [] #Contains a dictionary for each item
+
+class Item(Resource): #'Student' will inherit from 'Resource'
+    def get(self , name): #This resource can only be accessed with 'get' ... if you want 'post' add a post method
+        for item in items:
+            if item['name'] == name:
+                return item #We are using flask_restful, so no need to return JSON, it is done for you.
+        return {'item' : None }, 404
+
+    def post(self , name):
+        item = {'name' : name , 'price' : 12.00 }
+        items.append(item)
+        return item, 201
+
+#Passing in 'Student' tells 'Api' that 'Student' is accessible in the API.
+api.add_resource(Item , '/item/<string:name>')
+
+app.run(port=5000)
